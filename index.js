3ed81c72f47cdb5e054e4742ca6eff4d49646b86
ktrmzm <>
2024-01-27 22:50:51 +0900

    datetimeoriginal

--- index.js
+++ index.js
@@ -135,7 +135,7 @@ async function uploadFiles(imageFile) {
     sharp(imageFile)
   .metadata()
   .then(function(metadata) {
-    console.log(exif(metadata.exif).Image.DateTime)
+    console.log(exif(metadata.exif).Photo.DateTimeOriginal)
   })
   return;
     
