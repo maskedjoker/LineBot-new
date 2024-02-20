28201bfb2bdd3ea37ca39fec6a2d1a3f64b9e585
ktrmzm <k.maezmac@gmail.com>
2024-02-20 21:26:25 +0900

    カレンダー追加

--- index.js
+++ index.js
@@ -114,6 +114,27 @@ async function handleEvent(event) {
        return;
     }
 
+    const credentials = {
+        "type": "service_account",
+        "project_id": process.env.PROJECT_ID,
+        "private_key_id": process.env.PRIVATE_KEY_ID,
+        "private_key": process.env.PRIVATE_KEY.split(String.raw`\n`).join('\n'),
+        "client_email": process.env.CLIENT_EMAIL,
+        "client_id": process.env.CLIENT_ID,
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": process.env.CLIENT_X509_CRERT_URL,
+        "universe_domain": "googleapis.com"
+    };
+    const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
+    const auth = new google.auth.GoogleAuth({
+        scopes: SCOPES,
+        credentials: credentials,
+    });
+
+    await listEvents(auth);
+
     // const messages = [{
     //     type: 'text',
     //     text: 'ごいごいすー'
@@ -196,3 +217,25 @@ async function uploadFiles(stream, drive, dayDirectoryId, fileName, type) {
     }
     return dayDirectoryId;
 }
+
+async function listEvents(auth) {
+    const calendar = google.calendar({version: 'v3', auth});
+    const res = await calendar.events.list({
+      calendarId: 'primary',
+      timeMin: new Date().toISOString(),
+      maxResults: 10,
+      singleEvents: true,
+      orderBy: 'startTime',
+    });
+    const events = res.data.items;
+    if (!events || events.length === 0) {
+      console.log('No upcoming events found.');
+      return;
+    }
+    console.log('Upcoming 10 events:');
+    events.map((event, i) => {
+      const start = event.start.dateTime || event.start.date;
+      console.log(`${start} - ${event.summary}`);
+    });
+  }
+  
